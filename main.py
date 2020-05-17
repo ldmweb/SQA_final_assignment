8370b0ef1618c7b2e3e21f405d573d91568d1960
Lilian <lilian.desvaux98@gmail.com>
2020-05-18 00:18:29 +0100

    created create survey feature + test for this feature

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+class survey:
+    def __init__(self, name):
+        self.name = name
+
+
 class controller:
     def __init__(self):
         self.surveys = []
+
+    def createSurvey(self, name):
+        self.surveys.append(survey(name))
+        return "Survey created successfully"
