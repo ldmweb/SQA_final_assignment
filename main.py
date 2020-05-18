675d6f0414f8e81d903b45741073edc9fe39f0f0
Lilian <lilian.desvaux98@gmail.com>
2020-05-18 01:01:44 +0100

    created addQuestionSurvey method

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 class survey:
     def __init__(self, name):
         self.name = name
+        self.questions = []
+
+    def addQuestion(self, question):
+        if len(self.questions) < 10:
+            self.questions.append(question)
+            return "Successfully added the question to the survey"
+        else:
+            return "Reached limit of questions for this survey"
 
 
 class controller:
@@ -10,3 +18,10 @@ class controller:
     def createSurvey(self, name):
         self.surveys.append(survey(name))
         return ("Survey '" + name + "' created successfully")
+
+    def addQestionSurvey(self, name, question):
+        for survey self.surveys:
+            if survey.name == name:
+                return survey.addQuestion(question)
+            else:
+                return "Survey not found, please check the name of the survey"
